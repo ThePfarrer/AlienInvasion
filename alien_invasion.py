b8495957e4cb6356c28fb8df35d4d6cba0db7bad
Uche Okereke <orezpablo@gmail.com>
2020-11-19 16:59:23 +0100

    collision

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,13 +81,28 @@ class AlienInvasion:
 
     def _update_bullets(self):
         """Update position of bullets and get rid of old bullets."""
+
         # Update bullet positions.
         self.bullets.update()
+
         # Get rid of bullets that have disappeared.
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
             # print(len(self.bullets))
+
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update the positions of all aliens in the fleet."""
--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ class Settings:
         self.ship_speed = 1.5
 
         # Bullet settings 
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
